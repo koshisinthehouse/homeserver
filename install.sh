fcf47193cbc27bd994cfcedcd97540de54f8a9b7
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-18 15:52:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
-apt-get upgrade
-apt-get update
-apt-get install ethtool
-apt-get install net-tools
-
-
+sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get install ethtool -y
+sudo apt-get install net-tools -y
 # mac adresse ermitteln
- ip  link
-
-
 ifconfig
 # get network adapter infos
 sudo ip a
 # get network route and gateway adress
 netstat -rn
-
 # install docker - start
 # documentation online: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
 ## Install a few prerequisite packages which let apt use packages over HTTPS:
@@ -23,7 +17,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 ## Add the Docker repository to APT sources:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 ## Update your existing list of packages again for the addition to be recognized:
-sudo apt update
+sudo apt update -y
 ## Make sure you are about to install from the Docker repo instead of the default Ubuntu repo:
 apt-cache policy docker-ce
 ## Finally, install Docker:
@@ -31,6 +25,6 @@ sudo apt-get install docker-ce -y
 ## Docker should now be installed, the daemon started, and the process enabled to start on boot. Check that it’s running:
 sudo systemctl status docker
 # add user to docker group
-sudo usermod -aG docker ${USER}
-su - ${USER}
+sudo usermod -aG docker server
+su - server
 # install docker - end
